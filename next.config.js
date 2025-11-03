d0681d40fe2406fb3f72b8aefc7d1abbb781f4a8
Matvej Slyk <matvejslyk618@gmail.com>
2025-11-04 01:12:51 +0300

    Fix Netlify build config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Критично для Netlify!
+  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  distDir: 'out'
 }
 
 module.exports = nextConfig
